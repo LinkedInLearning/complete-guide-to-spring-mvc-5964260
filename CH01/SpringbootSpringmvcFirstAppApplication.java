fb1080df0f8a5204211f0e843b1670dba369435b
semirahd <semirah.dolan@gmail.com>
2024-12-16 22:52:12 -0500

    Add files via upload

---
+++ CH01/SpringbootSpringmvcFirstAppApplication.java
@@ -0,0 +1,15 @@
+package com.springmvctutorial.springboot_springmvc_first_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Main Spring Boot Class - as annotated by @SpringBootApplication
+// Hover over SpringBootApplication to learn more about it
+@SpringBootApplication
+public class SpringbootSpringmvcFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringmvcFirstAppApplication.class, args);
+	}
+
+}
